cf5e5d2e51bc669e39d0c38b9db18821189bc7f7
EliasGreen <akcuoma@rambler.ru>
2020-03-30 12:27:17 +0300

    Первая версия GUI

---
+++ TV.py
@@ -0,0 +1,74 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk, Image
+import os
+
+# --------------------------------
+# Главный класс - приложение
+# --------------------------------
+class App(object):
+    def __init__(self, master, **kwargs):
+        self.master = master
+
+        # Добавление главной картинки
+        self.canvas = Canvas(root, width=256, height=188)
+        self.canvas.place(relx=.5, rely=.3, anchor="c")
+        self.img = ImageTk.PhotoImage(Image.open("C:\\Users\\admin\\Desktop\\rsz_dicessex.png"))
+        self.canvas.create_image(0, 0, anchor=NW, image=self.img)
+
+        # Начальные графические параметры главного экрана приложения
+        master.title("Генератор типовых расчетов по предмету 'Теория вероятности'")
+        master.configure(background='gray')
+        master.geometry('600x450')
+
+        # Label кол-ва вариантов
+        vars_count_label = Label(text="Введите количество вариантов для генерации:")
+        vars_count_label.place(relx=.5, rely=.6, anchor="c")
+
+        # Input кол-ва вариантов
+        self.vars_count = StringVar()
+        vars_count_entry = Entry(textvariable=self.vars_count)
+        vars_count_entry.place(relx=.5, rely=.66, anchor="c")
+
+        # Кнопка обработки генерации вариантов
+        vars_gen_button = Button(text="Сгенерировать варианты", command=self.generate_variants)
+        vars_gen_button.place(relx=.5, rely=.8, anchor="c")
+
+        # Кнопка обработки удаления вариантов
+        vars_del_button = Button(text="Удалить сгенерированные варианты", command=self.delete_docx_variants)
+        vars_del_button.place(relx=.5, rely=.9, anchor="c")
+
+    # Вспомогательная функция вывода информации об успешности генерации вариантов
+    def show_gen_results(self):
+        if self.vars_count.get() != '' and int(self.vars_count.get()) > 0:
+            messagebox.showinfo("Успешная генерация\n ",
+                                   "Сгенерированные варианты находятся в "
+                                   "одной папке с программой.\n"
+                                   "Количество сгенерированных вариантов: " + self.vars_count.get())
+        elif self.vars_count.get() != '' and int(self.vars_count.get()) <= 0:
+            messagebox.showerror("Безуспешная генерация\n ",
+                                "Количество вариантов для генерации "
+                                "должно быть больше нуля")
+        elif self.vars_count.get() == '' :
+            messagebox.showerror("Безуспешная генерация\n ",
+                                "Вы не ввели количество вариантов для генерации")
+
+    # Метод запуска генерации объектов
+    def generate_variants(self):
+        self.show_gen_results()
+
+    # Метод запуска процесса удаления сгенерованных вариантов
+    # посредством удаления текстовых документов в текущей папке
+    # с опредленным паттерном названия
+    # TODO добавить паттерн и само удаление
+    def delete_docx_variants(self):
+        answer = messagebox.askyesno(title="Удаление сгенерированных вариантов", message="Удалить файлы типа docx?")
+        if answer:
+            messagebox.showwarning("Успешное удаление\n ",
+                                 "Файлы были удалены")
+
+
+root = Tk()
+app = App(root)
+root.wm_iconbitmap("C:\\Users\\admin\\Desktop\\favicon.ico")
+root.mainloop()
