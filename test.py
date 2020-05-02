5f0ce50841b76b05ab93e0139e1d9cecda8660af
EliasGreen <akcuoma@rambler.ru>
2020-05-02 12:38:16 +0300

    DONE: 13+14+15 tasks

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import random
+from numpy import sqrt, sin, cos, pi
 from scipy.special import erf
+import scipy.integrate as integrate
 Phi = lambda x: erf(x/2**0.5)/2
 
 
@@ -127,3 +129,90 @@ def task17():
           'ожидание, можем воспользоваться следующей формулой)')
     print(f'2Ф({shift_value}/{b_value}) = ')
     print(2*Phi(shift_value/b_value))
+
+
+def task13and14():
+    # чтобы добавить вариативности 13-ому заданию - достаточно добавить новую функцию в лист
+    # сначала идет функция -> потом ее собственный интеграл от нуля до x
+    all_functions = [('5cos(5x)', 'sin(5x)'), ('2x', 'x^2'), ('4*x^3','x^4')]
+    main_interval_points = ['PI/10', '1', '1']
+
+    selected_item = random.randint(0, len(all_functions) - 1)
+
+    # выводим условие задачи
+    print('Условие задачи:')
+    print('E - непрерывная случайная величина с плотностью распределения p(x), заданной следующим образом:')
+    print(f'p(x) = {all_functions[selected_item][0]}, для любого x, который принадлежит интервалу '
+          f'(0;{main_interval_points[selected_item]})')
+    print(f'p(x) = 0, для любого x, который не принадлежит интервалу '
+          f'(0;{main_interval_points[selected_item]})')
+    print('Нати функцию распределения F(x).')
+
+    # выводим решение
+    print('Решение:')
+    print('F(x) = 0, при x <= 0')
+    print(f'F(x) = {all_functions[selected_item][1]}, при 0 < x <= {main_interval_points[selected_item]}')
+    print(f'F(x) = 1, при x > {main_interval_points[selected_item]}')
+
+    # Далее идет условие/решение 14-ой задачи
+    print('\nУсловие задачи:')
+    print('E - непрерывная случайная величина примера 13.')
+    print('Найти M(E), D(E), б(E).\n')
+    M_E_values = [
+        integrate.quad(lambda x: x*5*cos(5*x), 0, pi/10),
+        integrate.quad(lambda x: x * (x*x), 0, 1),
+        integrate.quad(lambda x: x * 4 * x * x * x, 0, 1),
+                  ]
+
+    xx_F_values = [
+        integrate.quad(lambda x: x*x * 5 * cos(5 * x), 0, pi / 10),
+        integrate.quad(lambda x: x*x * (x*x), 0, 1),
+        integrate.quad(lambda x: x*x * 4 * x * x * x, 0, 1),
+    ]
+
+    D_E_values = [xx_F_values[i][0] - (M_E_values[i][0]*M_E_values[i][0]) for i in range(len(M_E_values))]
+    B_E_values = [sqrt(d_value) for d_value in D_E_values]
+
+    print('M(E) = ', M_E_values[selected_item][0])
+    print('D(E) = ', D_E_values[selected_item])
+    print('б(E) = ', B_E_values[selected_item])
+
+
+def task15():
+    n = 1
+    p = 1
+    q = 1
+    k = 1
+
+    while n*p*q <= 9 or not(sqrt(n*p*q).is_integer()):
+        n = random.randint(100, 10000)
+        p_values = [0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8]
+        p = random.choice(p_values)
+        q = (10.0 - p*10)/10.0
+
+        k = random.randint(45, n - 50)
+
+    phi_result = abs((k - n*p)/sqrt(n*p*q))
+
+
+    print(n, p, q)
+
+    print('Условие задачи:')
+    print(f'Вероятность наступления события А в одном опыте равна {p}.')
+    print(f'Найти вероятность того, что событие А наступит {k} раз в {n} опытах.')
+
+    print('Решение:')
+    print('Так как n*p*q много больше 9, то')
+    print('воспользуемся локальной теоремой лапласа')
+    print('Pn(K) = Phi(x)/sqrt(n*p*q)')
+    print('Где x = (k - np)/sqrt(n*p*q)')
+    print('Найдем значение x:')
+    print(f'x = ({k} - {n}*{p})/sqrt({(n*p*q)}) = {(k - n*p)/sqrt(n*p*q)}')
+    print('Т.к. Phi(x) - четная функция, то Phi(-x) = Phi(x)')
+    print(f'Phi({abs((k - n*p)/sqrt(n*p*q))}) = {Phi(abs((k - n*p)/sqrt(n*p*q)))}')
+    print(f'Тогда P = {Phi(abs((k - n*p)/sqrt(n*p*q)))/sqrt(n*p*q)}')
+
+task15()
+
+
+
